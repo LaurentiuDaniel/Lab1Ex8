2a930c9dcb1b6ca66aef1e39ffdf58a3576bf509
Dani <amaximoaiei.l@yahoo.com>
2023-06-24 21:30:31 +0300

    Add project files.

---
+++ Lab1Ex8/Lab1Ex8.cs
@@ -0,0 +1,24 @@
+﻿/*Ex 8
+Scrieti un program care interschimba valoarea a doua variabile intregi.*/
+
+class Lab1Ex8 {
+
+    static void Main() {
+
+        int primulNumar, alDoileaNumar, temp;
+
+        Console.WriteLine("Introdu prima valuare:");
+        primulNumar = int.Parse(Console.ReadLine());
+        Console.WriteLine("Introdu a doua valuare:");
+        alDoileaNumar = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ups programul meu a interschimbat valorile tale:");
+        temp = primulNumar;
+        primulNumar = alDoileaNumar;
+        alDoileaNumar = temp;
+        Console.WriteLine(primulNumar + "," + alDoileaNumar);
+    }
+
+}
+
+
